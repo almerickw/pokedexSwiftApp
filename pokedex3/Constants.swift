34cb4bf15c9f045d070d0349927f716a38192d09
Almerick <almerickwong@Almericks-MacBook-Pro.local>
2019-03-04 16:46:46 -0700

    Pokemon project reupload

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Almerick Wong on 2017-05-18.
+//  Copyright © 2017 Almerick Wong. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co/"
+let URL_POKEMON = "api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
